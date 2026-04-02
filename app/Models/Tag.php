8a773454d4720e723b5f47dd2a8f78a75bd57eee
Matt Benton <mattbenton@hey.com>
2026-04-02 16:10:27 -0500

    Add alphabetical name scope for tags

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\AlphabeticalNameScope;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+#[ScopedBy([AlphabeticalNameScope::class])]
 class Tag extends Model
 {
     /** @use HasFactory<\Database\Factories\TagFactory> */
