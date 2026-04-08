5be72b0a7a331fef4db2be9a66b2d685f8aaa54a
Matt Benton <mattbenton@hey.com>
2026-04-08 10:15:27 -0500

    Remove button shift transition

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
     <body class="bg-zinc-800 text-white flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
             @if (Route::has('login'))
-                <nav class="flex items-center justify-end gap-4 transition opacity-100 duration-1000 starting:opacity-0 delay-1600 starting:translate-x-2 ease-in">
+                <nav class="flex items-center justify-end gap-4 transition opacity-100 duration-1000 starting:opacity-0 delay-1600">
                     @auth
                         <a
                             href="{{ url('/dashboard') }}"
